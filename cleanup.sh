d4eafc1ee2bb5b5efd6eded47f604f3d55ff2f01
Andrew <andrewceng@gmail.com>
2020-07-23 21:16:58 +0700

    Updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -5,4 +5,9 @@ rm -fr elasticsearch/logs/* &&
 rm -fr logstash/data/* &&
 rm -fr kibana/data/* &&
 
+touch elasticsearch/data/.lock &&
+touch elasticsearch/logs/.lock &&
+touch logstash/data/.lock &&
+touch kibana/data/.lock &&
+
 echo "Data and logs are purged.  Dont' forget to git push"
