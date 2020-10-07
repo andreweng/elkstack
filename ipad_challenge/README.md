baace5a40959925296bb2d2fd71ec89b86920b61
Andrew <andrewceng@gmail.com>
2020-10-07 22:28:46 +0700

    created operational grabTweet script

---
+++ ipad_challenge/README.md
@@ -0,0 +1,7 @@
+# 90 Days iPad Challenge
+> Author: Andrew Eng | Date: 2020-10-05
+
+## The Challenge
+Today, my youngest daughter asked me a question I thought was pretty interesting.  She asked me; if you didn't have any money and had one thing, what what it be?  I instantly responded back with "laptop".  When asked why, I said, I can make more money with a laptop than anything else.  In theory, this makes sense.  Especially with all these millionaire youtubers, instagram influencers, and content creators out there.  But that's easier said than done.  It's easy to say "I can make money", but when it comes down to execution, that's where issues arise and excuses are made.  
+
+For the next 90 days, I will be only using my iPad and my goal is to generate passive income to survive as a single person.  In reality, I am married with 3 kids, have a steady income, and place to call home.  So for this challenge, I won't be utilizing any of the resources that I already have.  I will forgoe my weekly allowance for personal expenses and use only my iPad to "work".  
---
+++ use_case/grabTweets.py
@@ -0,0 +1,107 @@
+# Import tweepy as tw
+import tweepy as tw
+import sys
+from datetime import datetime as dt
+from elasticsearch import Elasticsearch
+
+def acqData(search, acq):
+
+    index_name = search.split(' ')[0] + '-' + dt.today().strftime('%Y-%m-%d')
+    feed = []
+    
+    print('::Acquiring Data::')
+   
+    for tweet in tw.Cursor(api.search, q=search, tweet_mode='extended').items(acq):
+        feed.append(tweet._json)
+
+    count = 0
+    
+    print('::Transferring to Elasticsearch Search::')
+    
+    while count < len(feed):
+        tweet_date = feed[count]['created_at']
+        username = feed[count]['user']['screen_name']
+        account_creation_date = feed[count]['created_at']
+        user_description = feed[count]['user']['description']
+        user_url = feed[count]['user']['url']
+        verified_status = feed[count]['user']['verified']
+        geo_enabled = feed[count]['user']['geo_enabled']
+        friends_count = feed[count]['user']['friends_count']
+        followers_count = feed[count]['user']['followers_count']
+        retweeted_count = feed[count]['retweet_count']
+        favorite_count = feed[count]['favorite_count']
+        hashtags = feed[count]['entities']['hashtags']
+        tweet_full_text = feed[count]['full_text']
+
+        doc = {
+            '@timestamp': dt.now(),
+            'tweet_date': tweet_date,
+            'username': str(username),
+            'account_creation_date': str(account_creation_date),
+            'user_description': str(user_description),
+            'user_url': str(user_url),
+            'verified_status': bool(verified_status),
+            'geo_enabled': bool(geo_enabled),
+            'friends_count': int(friends_count),
+            'followers_count': int(followers_count),
+            'retweeted_count': int(retweeted_count),
+            'favorite_count': int(favorite_count),
+            'hashtags': hashtags,
+            'tweet_full_text': str(tweet_full_text),
+            'word_list': str(tweet_full_text).split(' ')
+        }
+
+        es.index(index=index_name, body=doc)
+        
+        count +=1
+    
+    print(f'Processed {tweet_count} records of {search} to {server}')
+   
+def help():
+    print('usage: grabTweets.py "palantir OR PLTR" 2500 "172.16.100.25"')
+    print(f'Search + Number to Tweets to Grab + Elasticsearch Server')
+
+try:
+    search = sys.argv[1] 
+    tweet_count = sys.argv[2]
+    server = sys.argv[3]
+
+    # Import keys from a saved file instead of inputting it directly into the script.  
+    # Strip whitespaces and split on = as I only want the key values
+
+    key_location = 'twitter.keys'
+    apikeys = []
+    with open(key_location) as keys:
+        for i in keys:
+            apikeys.append(i.split("=")[1].strip(" ").strip("\n"))
+    keys.close()
+
+    # Initialize dictionary
+    twitter_cred = dict()
+
+    # Enter API keys
+    twitter_cred["CONSUMER_KEY"] = apikeys[0]
+    twitter_cred["CONSUMER_SECRET"] = apikeys[1]
+
+    # Access Tokens
+    twitter_cred["ACCESS_KEY"] = apikeys[2]
+    twitter_cred["ACCESS_SECRET"] = apikeys[3]
+
+    # Set authentication object
+
+    auth = tw.OAuthHandler(twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
+    auth.set_access_token(twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
+
+    # Create api object with authentication
+
+    api = tw.API(auth, wait_on_rate_limit=True)
+
+    es = Elasticsearch(server, port=9200)
+
+    acqData(str(search), int(tweet_count))
+
+except FileNotFoundError:
+    print(f'\n !!! You need to create a twitter.keys file !!!\n')
+except IndexError:
+    print(f'\n !!! You need to add some arguments!!!')
+    help()
