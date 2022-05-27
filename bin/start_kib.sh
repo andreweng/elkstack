3a5d5b7506e040cfed2503bbd3ee19cf32f171be
Andrew Eng <andreweng@Andrews-MacBook-Pro.local>
2022-05-27 13:01:36 -0400

    elkstack version upgrade and replaced directory sharing with volumes

--- bin/start_kib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run \
-    -p 5601:5601 \
-    -v ${PWD}/kibana/config:/usr/share/kibana/config \
-    -v ${PWD}/kibana/data:/usr/share/kibana/data \
-docker.elastic.co/kibana/kibana:7.8.0
--- bin/start_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run \
-    -p 5141:5141 \
-    -v ${PWD}/logstash/config:/usr/share/logstash/config \
-    -v ${PWD}/logstash/pipeline:/usr/share/logstash/pipeline \
-    -v ${PWD}/logstash/data:/usr/share/logstash/data \
-docker.elastic.co/logstash/logstash:7.8.0
